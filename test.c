1ff54c58ea6e2d32e78d218de68dddf613cd0b5b
test <git@github.com>
2024-08-16 06:39:27 +0800

    123

---
+++ test.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello VSCode!\n");
+    getchar();
+    return 0;
+}
+
+// 如果能成功运行（ctrl+F5），恭喜您！
+// 如果还是不行，请到B站视频(https://www.bilibili.com/video/BV1Fh4y167Lv)评论区描述您的具体问题或者发送邮件至wait_er@tju.edu.cn，作者将为您解答
+//     _-----_     ╭──────────────────────────╮
+//    |       |    │   Welcome to the Visual  │
+//    |--(o)--|    │   Studio Code Extension  │
+//   `---------´   │        generator!        │
+//    ( _´U`_ )    ╰──────────────────────────╯
+//    /___A___\   /
+//     |  ~  |
+//   __'.___.'__
+// ´   `  |° ´ Y `
+// 作者博客(https://ymake1.github.io)，有扩展的优化建议也欢迎与作者联系(wait_er@tju.edu.cn)
+// 祝您Visual Studio Code旅途愉快！
